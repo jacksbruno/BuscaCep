1d85de8693a2f99b5e62d3bb72a001ff70283e16
Fernando Arce <fernando.arce@MacBook-Pro-de-Fernando.local>
2022-08-05 14:21:16 -0400

    implementacao das funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   StyleSheet,
   View,
@@ -6,23 +6,39 @@ import {
   TextInput,
   TouchableOpacity,
   SafeAreaView,
+  Keyboard,
 } from 'react-native';
 import api from './services/api';
 
 export default App = () => {
   const [cep, setCep] = useState(null)
-  const [cepDigitado, setCepDigitado] = useState(null)
-  const [endereco, setEndereco] = useState([])
+  const [endereco, setEndereco] = useState({})
+  const [inputFocus, setInputFocus] = useState(false)
+  const inputRef = useRef(null)
 
   
 
-  function buscarCep(){
-    setCepDigitado(cep)
-    api.get(`/${Number(cepDigitado)}/json`).then(res => {
-      setEndereco(res)
-      console.log('endereco', endereco)
-    })
+  async function buscarCep(){
+    Keyboard.dismiss()
+    if(cep.length < 8){
+      alert('Digite um CEP válido!')
+      return false
+    }
 
+    try{
+      const resp = await api.get(`/${Number(cep)}/json`)
+      setEndereco(resp.data)
+      console.log('resp',resp.data)
+    }catch(err){
+      console.log('ERROR: '+err)
+    }
+
+  }
+
+  function limpar(){
+    setCep('')
+    setEndereco({})
+    inputRef.current.focus()
   }
 
   return(
@@ -37,44 +53,58 @@ export default App = () => {
           keyboardType="numeric"
           value={cep}
           placeholder="Ex: 79002563"
+          maxLength={8}
+          ref={inputRef}
+          onFocus={() => setInputFocus(true)}
+          onBlur={() => setInputFocus(false)}
         />
       </View>
       <View style={styles.areaBtn}>
         <TouchableOpacity style={styles.btnBusca} onPress={buscarCep}>
           <Text style={styles.txtBtn}>Buscar</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.btnBusca, { backgroundColor:'#FF7C6B', marginLeft: 20 }]} >
+        <TouchableOpacity style={[styles.btnBusca, { backgroundColor:'#FF7C6B', marginLeft: 20 }]} onPress={limpar} >
           <Text style={styles.txtBtn}>Limpar</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.areaBusca}>
-        <View style={{width: '90%', justifyContent:'flex-start', marginBottom: 5}}>
-          <Text style={styles.labelResultado}>Resultado da Busca</Text>
+      {
+        endereco.logradouro&&!inputFocus
+        ?
+        <View style={styles.areaBusca}>
+          <View style={{width: '90%', justifyContent:'flex-start', marginBottom: 5}}>
+            <Text style={styles.labelResultado}>Resultado da Busca</Text>
+          </View>
+          <View style={styles.conteudoBusca}>
+            <View style={styles.linha}>
+              <Text style={styles.labelLinha}>CEP:</Text>
+              <Text style={styles.conteudoLinha}>{ endereco.cep }</Text>
+            </View>
+            <View style={styles.linha}>
+              <Text style={styles.labelLinha}>Logradouro:</Text>
+              <Text style={styles.conteudoLinha}>{ endereco.logradouro }</Text>
+            </View>
+            <View style={styles.linha}>
+              <Text style={styles.labelLinha}>Complemento:</Text>
+              <Text style={styles.conteudoLinha}>{ endereco.complemento }</Text>
+            </View>
+            <View style={styles.linha}>
+              <Text style={styles.labelLinha}>Bairro:</Text>
+              <Text style={styles.conteudoLinha}>{ endereco.bairro }</Text>
+            </View>
+            <View style={styles.linha}>
+              <Text style={styles.labelLinha}>Cidade:</Text>
+              <Text style={styles.conteudoLinha}>{ endereco.localidade }</Text>
+            </View>
+            <View style={styles.linha}>
+              <Text style={styles.labelLinha}>Estado:</Text>
+              <Text style={styles.conteudoLinha}>{ endereco.uf }</Text>
+            </View>
+          </View>
         </View>
-        <View style={styles.conteudoBusca}>
-          <View style={styles.linha}>
-            <Text style={styles.labelLinha}>CEP:</Text>
-            <Text style={styles.conteudoLinha}>79011060</Text>
-          </View>
-          <View style={styles.linha}>
-            <Text style={styles.labelLinha}>Logradouro:</Text>
-            <Text style={styles.conteudoLinha}>Rua Doutor Meireles</Text>
-          </View>
-          <View style={styles.linha}>
-            <Text style={styles.labelLinha}>Bairro:</Text>
-            <Text style={styles.conteudoLinha}>Monte Castelo</Text>
-          </View>
-          <View style={styles.linha}>
-            <Text style={styles.labelLinha}>Cidade:</Text>
-            <Text style={styles.conteudoLinha}>Campo Grande</Text>
-          </View>
-          <View style={styles.linha}>
-            <Text style={styles.labelLinha}>Estado:</Text>
-            <Text style={styles.conteudoLinha}>Mato Grosso do Sul</Text>
-          </View>
-        </View>
-      </View>
+        :
+        false
+      }
     </SafeAreaView>
   )
 }
@@ -152,7 +182,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: 'bold',
     color: '#5F30E1',
-    width: 100
+    width: 115
   },
   conteudoLinha: {
     fontSize: 14,
